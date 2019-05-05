02066f9f9924842844dd64848e81621e360b1566
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2019-05-05 15:29:50 -0700

    Google Code Jam Template

---
+++ Template/Template.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <array>
+#include <utility>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <climits>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <set>
+#include <map>
+#include <list>
+#include <numeric>
+#include <unordered_set>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution
+{
+public:
+
+};
+
+int main(void)
+{
+	int T;
+	cin >> T;
+	for (int t = 1; t <= T; ++t)
+	{
+		cout << "Case #" << t << ": " << "Answer" << endl;
+	}
+	return 0;
+}
